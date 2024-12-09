17865c749553c89321c9c22c2c56a44c08eed30f
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2024-12-09 16:49:30 -0300

    Implement the Email sender service

--- book-network/src/main/java/com/devsouzx/book_network/auth/AuthenticationService.java
+++ book-network/src/main/java/com/devsouzx/book_network/auth/AuthenticationService.java
@@ -1,11 +1,15 @@
 package com.devsouzx.book_network.auth;
 
+import com.devsouzx.book_network.email.EmailService;
+import com.devsouzx.book_network.email.EmailTemplateName;
 import com.devsouzx.book_network.role.RoleRepository;
 import com.devsouzx.book_network.user.Token;
 import com.devsouzx.book_network.user.TokenRepository;
 import com.devsouzx.book_network.user.User;
 import com.devsouzx.book_network.user.UserRepository;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,8 +24,12 @@ public class AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final TokenRepository tokenRepository;
+    private final EmailService emailService;
 
-    public void register(RegistrationRequest request) {
+    @Value("${application.mailing.frontend.activation-url}")
+    private String activationUrl;
+
+    public void register(RegistrationRequest request) throws MessagingException {
         var userRole = roleRepository.findByName("USER")
                 .orElseThrow(() -> new IllegalStateException("ROLE USER was not initiated"));
         var user = User.builder()
@@ -37,9 +45,17 @@ public class AuthenticationService {
         sendValidationEmail(user);
     }
 
-    private void sendValidationEmail(User user) {
+    private void sendValidationEmail(User user) throws MessagingException {
         var newToken = generateAndSaveActivationToken(user);
-        // send email
+
+        emailService.sendEmail(
+                user.getEmail(),
+                user.getFullName(),
+                EmailTemplateName.ACTIVATE_ACCOUNT,
+                activationUrl,
+                newToken,
+                "Account activation"
+        );
     }
 
     private String generateAndSaveActivationToken(User user) {
