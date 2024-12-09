e18aee86c0fe1764b7e2c196861c35ebb44225d5
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2024-12-09 15:42:32 -0300

    Create the authentication controller

---
+++ book-network/src/main/java/com/devsouzx/book_network/auth/AuthenticationService.java
@@ -0,0 +1,10 @@
+package com.devsouzx.book_network.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    public void register(RegistrationRequest request) {
+
+    }
+}
