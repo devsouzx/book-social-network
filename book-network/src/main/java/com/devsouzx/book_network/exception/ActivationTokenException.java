a7c6602eb188f74d2aa4693e19f63c224c1e8d69
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2024-12-13 19:21:20 -0300

    Exception handling

---
+++ book-network/src/main/java/com/devsouzx/book_network/exception/ActivationTokenException.java
@@ -0,0 +1,7 @@
+package com.devsouzx.book_network.exception;
+
+public class ActivationTokenException extends RuntimeException {
+    public ActivationTokenException(String message) {
+        super(message);
+    }
+}
