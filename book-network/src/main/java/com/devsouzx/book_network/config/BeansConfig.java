cdf063c8f9fd10585524e78288dd2a8c1f5c21b6
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2024-12-17 15:24:38 -0300

    Add the application auditor aware:: Auditing

--- book-network/src/main/java/com/devsouzx/book_network/config/BeansConfig.java
+++ book-network/src/main/java/com/devsouzx/book_network/config/BeansConfig.java
@@ -4,6 +4,7 @@ import com.devsouzx.book_network.auth.AuthenticationController;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -28,6 +29,11 @@ public class BeansConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return new ApplicationAuditAware();
     }
 
     @Bean
