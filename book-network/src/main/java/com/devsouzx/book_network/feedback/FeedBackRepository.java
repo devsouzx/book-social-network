c074b4eefb6c2d87714650cc0c2577aca232a3fa
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2024-12-18 18:36:34 -0300

    Implement save feedback

---
+++ book-network/src/main/java/com/devsouzx/book_network/feedback/FeedBackRepository.java
@@ -0,0 +1,6 @@
+package com.devsouzx.book_network.feedback;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedBackRepository extends JpaRepository<Feedback, Integer> {
+}
---
+++ book-network/src/main/java/com/devsouzx/book_network/feedback/FeedbackService.java
@@ -0,0 +1,32 @@
+package com.devsouzx.book_network.feedback;
+
+import com.devsouzx.book_network.book.Book;
+import com.devsouzx.book_network.book.BookRepository;
+import com.devsouzx.book_network.exception.OperationNotPermittedException;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class FeedbackService {
+    private final FeedBackRepository feedBackRepository;
+    private final BookRepository bookRepository;
+    private final FeedbackMapper feedbackMapper;
+
+    public Integer save(FeedbackRequest request, Authentication connectedUser) {
+        Book book = bookRepository.findById(request.bookId())
+                .orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + request.bookId()));
+        if (book.isArchived() || !book.isShareable()) {
+            throw new OperationNotPermittedException("You cannot give a feedback for and archived or not shareable book");
+        }
+        if (Objects.equals(book.getCreatedBy(), connectedUser.getName())) {
+            throw new OperationNotPermittedException("You cannot give feedback to your own book");
+        }
+        Feedback feedback = feedbackMapper.toFeedback(request);
+        return feedBackRepository.save(feedback).getId();
+    }
+}
