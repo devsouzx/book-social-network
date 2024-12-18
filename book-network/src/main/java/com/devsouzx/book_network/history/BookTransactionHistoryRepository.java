7577d774f24e09e6c4f080eecfbea203d955c262
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2024-12-18 17:09:09 -0300

    Implement the find all returned books

--- book-network/src/main/java/com/devsouzx/book_network/history/BookTransactionHistoryRepository.java
+++ book-network/src/main/java/com/devsouzx/book_network/history/BookTransactionHistoryRepository.java
@@ -12,4 +12,11 @@ public interface BookTransactionHistoryRepository extends JpaRepository<BookTran
             WHERE history.userId = :userId
             """)
     Page<BookTransactionHistory> findAllBorrowedBooks(Pageable pageable, String  userId);
+
+    @Query("""
+            SELECT history
+            FROM BookTransactionHistory history
+            WHERE history.book.createdBy = :userId
+            """)
+    Page<BookTransactionHistory> findAllReturnedBooks(Pageable pageable, String userId);
 }
