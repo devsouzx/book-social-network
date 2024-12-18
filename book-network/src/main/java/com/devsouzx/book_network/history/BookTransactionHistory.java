672cec0a18658dd71b8c653f6455fce33c5fc511
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2024-12-18 17:38:16 -0300

    Implement borrow book feature

--- book-network/src/main/java/com/devsouzx/book_network/history/BookTransactionHistory.java
+++ book-network/src/main/java/com/devsouzx/book_network/history/BookTransactionHistory.java
@@ -20,9 +20,8 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @Entity
 public class BookTransactionHistory extends BaseEntity {
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+    @Column(name = "user_id")
+    private String userId;
 
     @ManyToOne
     @JoinColumn(name = "book_id")
--- book-network/src/main/java/com/devsouzx/book_network/history/BookTransactionHistoryRepository.java
+++ book-network/src/main/java/com/devsouzx/book_network/history/BookTransactionHistoryRepository.java
@@ -4,8 +4,28 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BookTransactionHistoryRepository extends JpaRepository<BookTransactionHistory, Integer> {
+    @Query("""
+            SELECT
+            (COUNT (*) > 0) AS isBorrowed
+            FROM BookTransactionHistory bookTransactionHistory
+            WHERE bookTransactionHistory.userId = :userId
+            AND bookTransactionHistory.book.id = :bookId
+            AND bookTransactionHistory.returnApproved = false
+            """)
+    boolean isAlreadyBorrowedByUser(@Param("bookId") Integer bookId, @Param("userId") String userId);
+
+    @Query("""
+            SELECT
+            (COUNT (*) > 0) AS isBorrowed
+            FROM BookTransactionHistory bookTransactionHistory
+            WHERE bookTransactionHistory.book.id = :bookId
+            AND bookTransactionHistory.returnApproved = false
+            """)
+    boolean isAlreadyBorrowed(@Param("bookId") Integer bookId);
+
     @Query("""
             SELECT history
             FROM BookTransactionHistory history
