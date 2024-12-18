dfa89f1ae6cc2e43a8826ebeb53105110c423be8
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2024-12-18 17:48:07 -0300

    Implement approve book return

--- book-network/src/main/java/com/devsouzx/book_network/history/BookTransactionHistoryRepository.java
+++ book-network/src/main/java/com/devsouzx/book_network/history/BookTransactionHistoryRepository.java
@@ -39,6 +39,16 @@ public interface BookTransactionHistoryRepository extends JpaRepository<BookTran
     Optional<BookTransactionHistory> findByBookIdAndUserId(@Param("bookId") Integer bookId, @Param("userId") String userId);
 
     @Query("""
+            SELECT transaction
+            FROM BookTransactionHistory  transaction
+            WHERE transaction.book.createdBy = :userId
+            AND transaction.book.id = :bookId
+            AND transaction.returned = true
+            AND transaction.returnApproved = false
+            """)
+    Optional<BookTransactionHistory> findByBookIdAndOwnerId(@Param("bookId") Integer bookId, @Param("userId") String userId);
+
+    @Query("""
             SELECT history
             FROM BookTransactionHistory history
             WHERE history.userId = :userId
